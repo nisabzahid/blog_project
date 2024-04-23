c351aa7947493608bcddad85e7040302979ace97
nisab zahid <nz@tsl.io>
2024-04-23 10:55:29 +0600

    added tagging funtionality

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
-# Create your models here.
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -21,7 +21,7 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
     author = models.ForeignKey(User,on_delete=models.CASCADE, related_name='blog_posts')
-    
+    tags = TaggableManager()
     objects = models.Manager()
     published = PublishedManager() 
     
--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -6,12 +6,32 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from .forms import EmailPostForm, CommentForm
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
-class PostListView(ListView):
-    queryset = Post.objects.all()
-    context_object_name = 'posts'
-    paginate_by = 3
-    template_name = 'blogapp/post/list.html'
+# class PostListView(ListView):
+#     queryset = Post.objects.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blogapp/post/list.html'
+
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+        # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(request,'blogapp/post/list.html',{'posts': posts,'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
